08edad99dbf78ea31fa3092be03e634db242c8f0
Luiz Hupalo <lhpzl@protonmail.com>
2023-03-02 10:31:06 -0300

    refactoring code

--- modules/mpsk.py
+++ modules/mpsk.py
@@ -21,33 +21,24 @@ def qpsk(num_bits, EbN0_dBs):
 
     for i in range(len(EbN0_dBs)):
 
-        # Vetor de bits a ser transmitido
-        x = np.round(np.random.rand(num_bits))
+        info = np.random.randint(0, 4, num_symbols)
 
-        # Modulação do sinal
-        j = np.arange(0, len(x), 2)
-        mask1 = np.logical_and(x[j] == 0, x[j+1] == 0)
-        mask2 = np.logical_and(x[j] == 0, x[j+1] == 1)
-        mask3 = np.logical_and(x[j] == 1, x[j+1] == 1)
-        mask4 = np.logical_and(x[j] == 1, x[j+1] == 0)
+        """
+        Mapeamento Gray 4-PSK (QPSK):
+        
+            - Bit 00 = Símbolo -1+1j (portadora com fase phi = +135)
+            - Bit 01 = Símbolo +1+1j (portadora com fase phi = +45)
+            - Bit 11 = Símbolo +1-1j (portadora com fase phi = -45)
+            - Bit 10 = Símbolo -1-1j (portadora com fase phi = -135)
+        """
+        constelacao = np.array([1+0j, 0+1j, -1+0j, 0-1j])
 
-        rl = np.zeros(len(j))
-        img = np.zeros(len(j))
-
-        rl[mask1] = 1
-        img[mask1] = 0
-
-        rl[mask2] = 0
-        img[mask2] = 1
-
-        rl[mask3] = -1
-        img[mask3] = 0
-
-        rl[mask4] = 0
-        img[mask4] = -1
-
-        # Sinal transmitido obtido através da modulação do vetor de bits de informação
-        s = rl + 1j*img
+        # Sinal de informação a ser transmitido
+        bits = np.array([[0,1],[0,0],[1,0],[1,1]])
+        x = bits[info].reshape(-1)
+        
+        # Sinal transmitido através da modulação do vetor de bits de informação
+        s = constelacao[info]
         
         # Variância do sinal transmitido e adição de ruído AWGN
         var = (1/np.sqrt(4))*10**(-EbN0_dBs[i]/20)
@@ -69,21 +60,21 @@ def qpsk(num_bits, EbN0_dBs):
         d_s = []
         for j in range(len(phi)):
             
-            if (bool(phi[j] >= 0) & bool(phi[j] <= 45)) | bool(phi[j] > 315): # Símbolo 1 -> 00
-                b1 = 0
-                b2 = 0
-                c = complex(b1,b2)
-            elif (bool(phi[j] > 45) & bool(phi[j] < 135)): # Símbolo 2 -> 01
+            if (bool(phi[j] >= 0) & bool(phi[j] <= 45)) | bool(phi[j] > 315): # Símbolo 1 -> 01 -> 1+0j
                 b1 = 0
                 b2 = 1
                 c = complex(b1,b2)
-            elif (bool(phi[j] > 135) & bool(phi[j] < 225)): # Símbolo 3 -> 11
-                b1 = 1
-                b2 = 1
+            elif (bool(phi[j] > 45) & bool(phi[j] < 135)): # Símbolo 2 -> 00 -> 0+j1
+                b1 = 0
+                b2 = 0
                 c = complex(b1,b2)
-            elif (bool(phi[j] > 225) & bool(phi[j] < 315)): # Símbolo 4 -> 10
+            elif (bool(phi[j] > 135) & bool(phi[j] < 225)): # Símbolo 3 -> 10 -> -1+j0
                 b1 = 1
                 b2 = 0
+                c = complex(b1,b2)
+            elif (bool(phi[j] > 225) & bool(phi[j] < 315)): # Símbolo 4 -> 11 -> 0-j1
+                b1 = 1
+                b2 = 1
                 c = complex(b1,b2)
         
             d.append([b1, b2])
@@ -120,16 +111,26 @@ def bpsk(num_bits, EbN0_dBs):
         BER (array): Taxa de erro de bit simulada
         SER (array): Taxa de erro de símbolo simulada
     """ 
-
     num_errors = np.zeros(len(EbN0_dBs))
 
     for i in range(len(EbN0_dBs)):
 
-        # Vetor de bits a ser transmitido
-        x = np.round(np.random.rand(num_bits))
+        info = np.random.randint(0, 2, num_bits)
 
-        # Sinal transmitido obtido através da modulação do vetor de bits de informação
-        s = np.round(2*x-1)
+        """
+        Mapeamento 2-PSK (BPSK):
+        
+            - Bit 0 = Símbolo -1 (portadora com fase phi = 0)
+            - Bit 1 = Símbolo +1 (portadora com fase phi = 180)
+        """
+        constelacao = np.array([-1, 1])
+
+        # Sinal de informação a ser transmitido
+        bits = np.array([0,1])
+        x = bits[info].reshape(-1)
+        
+        # Sinal transmitido através da modulação do vetor de bits de informação
+        s = constelacao[info]
 
         # Variância do sinal transmitido e adição de ruído AWGN
         var = (1/np.sqrt(2))*10**(-EbN0_dBs[i]/20)
